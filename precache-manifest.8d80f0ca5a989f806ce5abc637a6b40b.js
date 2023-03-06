89b4d91123a52a67ca4b3bddc694e92c582eef72
hdzylovesyou <106484628+hdzy@users.noreply.github.com>
2023-03-06 04:56:00 +0300

    Add files via upload

---
+++ precache-manifest.8d80f0ca5a989f806ce5abc637a6b40b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e8d96c401c66aa55911",
+    "url": "./static/css/main.1e4b581e.chunk.css"
+  },
+  {
+    "revision": "4e8d96c401c66aa55911",
+    "url": "./static/js/main.4e8d96c4.chunk.js"
+  },
+  {
+    "revision": "253343bce35cbc723b1b",
+    "url": "./static/js/1.253343bc.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "8ded93d8933029cc0f76721cfd87200b",
+    "url": "./static/media/arrow-down.8ded93d8.svg"
+  },
+  {
+    "revision": "48c7131693f5b8b987c586623906cead",
+    "url": "./index.html"
+  }
+];
